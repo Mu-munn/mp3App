ae25de0016946911e77e96186df6047de1385f38
MU-munn <rpgmukai333@gmail.com>
2022-10-12 15:14:16 +0900

    変えてみたよくわからん

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const testObjData = {
+  textObject1: {
+    title: "Objectのタイトル1です",
+    subTitle: "Objectのサブタイトル1です",
+    bodyText: "Objectの本文1です",
+  },
+  textObject2: {
+    title: "Objectのタイトル2です",
+    subTitle: "Objectのサブタイトル2です",
+    bodyText: "Objectの本文2です",
+  },
+};
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.json(testObjData);
 });
 
 module.exports = router;
