44abf9c899aba1b18c4d4e4f2c5e85253d0cd4d0
向川原 悠貴 <rpgmukai333@gmail.com>
2022-10-15 16:44:06 +0900

    re init app

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const router = require('./routes/v1')
+app.use("/api/v1",router)
+
+// start server
+app.listen(port);
+console.log(`listen on : ${port}`);
